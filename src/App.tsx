a045abcdb9d6a30db85837b6d359b74c5e6efade
Oumy-crypto <oumybasse2000@gmail.com>
2026-03-03 00:19:00 +0000

    basename fix

--- src/App.tsx
+++ src/App.tsx
@@ -14,7 +14,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter basename="/to-do-list-apps">
+      <BrowserRouter basename="/to-do-list-apps/">
         <Routes>
           <Route path="/" element={<Index />} />
           <Route path="/todo" element={<Todo />} />
